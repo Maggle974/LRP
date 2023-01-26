b6eaf37a65e7196b521ede2e3de0ca73a42a6379
vincent le callonnec <lecallonnec.v@gmail.com>
2023-01-26 14:40:10 +0100

    creating app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LRP::Application.config.secret_token = '3d5864913e9fb1332b44428fdd895c9f6bcf4cc99e2e1fae86631cc361c8aa017c6c4537b7eef8032a6fa9ab7a59d188900ba99137a5f64ae320d51a0eda0ad2'
